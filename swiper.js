4058adc07efb4d2fcd854f4e42a27170c96e3926
mabirhire1 <mabirhire1@gmail.com>
2023-10-17 09:53:20 +0100

    OMAX

---
+++ swiper.js
@@ -0,0 +1,102 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 1000,
+    direction: 'horizontal',
+         
+    //keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    //mousewheel
+    mousewheel: {
+      invert: true,
+    },
+
+    loop: true,
+     //autp play
+    autoplay: {
+      delay: 2000,
+    },
+
+    //  breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    //when the window width is >=1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+    //when the window width is >=2560px
+    2560: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+  });
+
+  
+  // swiper1 configuration
+  const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    speed: 1000,
+    direction: 'horizontal',
+       
+    //keyboard
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    //mousewheel
+    mousewheel: {
+      invert: true,
+    },
+
+    loop: true,
+     //autp play
+    autoplay: {
+      delay: 2000,
+    },
+
+    //  breakpoints
+    breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    //when the window width is >=1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+
+    //when the window width is >=2560px
+    2560: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+  });
+   
